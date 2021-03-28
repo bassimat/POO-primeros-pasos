a568a7488696b6d03f70ab9190df12fb2ea9ed2c
BASSIMAT <bassi_matias@redlink.com.ar>
2021-03-28 18:26:55 -0300

    Commit inicial

---
+++ DLL_Restaurante/Plato.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_Restaurante
+{
+    class Plato
+    {
+        private string _nombre;
+        private int _precioBase;
+
+        public void Plato()
+        {
+        }
+
+        public void Plato( string nombre, int cantDeIntegrantes )
+        {
+        }
+    }
+}
---
+++ Restaurante/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_Restaurante
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+
+            int [] vector = new int[1];
+            vector[0] = 10;
+            vector[1] = 20;
+        }
+    }
+}

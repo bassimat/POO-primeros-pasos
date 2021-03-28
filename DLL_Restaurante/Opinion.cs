b7888b63f82af65c50c9c91b7cced04604929426
BASSIMAT <bassi_matias@redlink.com.ar>
2021-03-28 20:03:17 -0300

    Agrega clase Opinion para agregar y promediar puntos

---
+++ DLL_Restaurante/Opinion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_Restaurante
+{
+    class Opinion
+    {
+        private const int PUNTAJE_MAXIMO = 5;
+        private List<int> _puntajes = new List<int>();
+
+        public void AgregarOpinion( int unPuntaje )
+        {
+            if ( unPuntaje < 0 || unPuntaje > PUNTAJE_MAXIMO )
+                throw new ArgumentOutOfRangeException();
+
+            this._puntajes.Add( unPuntaje );
+        }
+
+        public double GetOpinionPromedio()
+        {
+            double puntajeAcumulado = 0;
+            foreach (int puntaje in this._puntajes) {
+		        puntajeAcumulado += puntaje;
+            }
+            return puntajeAcumulado/this._puntajes.Count;
+	    }
+    }
+}
